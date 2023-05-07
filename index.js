ec8aabecd41e0389b01a8f72a4eb62b5eef6bbf7
Naimur Rahman <naimur2231@gmail.com>
2023-05-08 00:41:18 +0600

    porject done

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+
+const users = [
+    {id: '1', name: 'Naimur Rahman', email: 'naimur@gmail.com'},
+    {id: '2', name: 'Sakil Ahammed', email: 'sakil@gmail.com'},
+    {id: '3', name: 'Tanvir Rahman', email: 'tanvir@gmail.com'}
+]
 
 app.get('/', (req, res) => {
-    res.send('users management server is running!');
+    res.send('users management server is running!!');
+})
+
+app.get('/users', (req, res) => {
+    res.send(users);
+})
+
+app.post('/users', (req, res) => {
+    
+    console.log(req.body);
+    const newUser = req.body;
+     newUser.id = users.length + 1;
+     users.push(newUser)
+     res.send(newUser)
 })
 
 app.listen(port, () => {
